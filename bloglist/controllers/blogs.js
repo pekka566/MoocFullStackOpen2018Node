9876c8259dfb4d9ac869e3da2d07a7691c4bc77b
pekka566 <pekka.suomala@gmail.com>
2018-06-01 13:20:46 +0300

    Adding 7.6. comments - part 1.

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -1,63 +1,69 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog
-    .find({})
-    .populate('user', { username: 1, name: 1 } )
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
 
-  response.json(blogs.map(Blog.format))
-})
+  response.json(blogs.map(Blog.format));
+});
+
+blogsRouter.post('/:id/comments', async (request, response) => {
+  console.log('update: ' + request);
+
+  const blog = await Blog.findById(request.params.id);
+  blog.comments = [...blog.comments, request.body.comment];
+  const updatedBlog = await blog.save();
+  response.json(updatedBlog);
+});
 
 blogsRouter.post('/', async (request, response) => {
-  const token = request.token
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  const token = request.token;
+  const decodedToken = jwt.verify(token, process.env.SECRET);
 
   if (!token || !decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
+    return response.status(401).json({ error: 'token missing or invalid' });
   }
 
   if (request.body.title === undefined) {
-    return response.status(400).json({ error: 'title missing' })
+    return response.status(400).json({ error: 'title missing' });
   }
 
   if (request.body.url === undefined) {
-    return response.status(400).json({ error: 'url missing' })
+    return response.status(400).json({ error: 'url missing' });
   }
 
-  const user = await User.findById(decodedToken.id)
-  const {title, author, url, userId } = request.body
-  const likes = request.body.likes ? request.body.likes : 0
+  const user = await User.findById(decodedToken.id);
+  const { title, author, url, userId } = request.body;
+  const likes = request.body.likes ? request.body.likes : 0;
 
   const blog = new Blog({
-      title,
-      author,
-      url,
-      likes,
-      user: user._id
-    }
-  )
+    title,
+    author,
+    url,
+    likes,
+    user: user._id
+  });
 
-  const newBlog = await blog.save()
-  user.blogs = user.blogs.concat(newBlog._id)
-	await user.save()
+  console.log(blog);
 
-  response
-    .status(201)
-    .json(newBlog)
-})
+  const newBlog = await blog.save();
+  user.blogs = user.blogs.concat(newBlog._id);
+  await user.save();
+
+  response.status(201).json(newBlog);
+});
 
 blogsRouter.delete('/:id', async (request, response) => {
   try {
-    await Blog.findByIdAndRemove(request.params.id)
+    await Blog.findByIdAndRemove(request.params.id);
 
-    response.status(204).end()
+    response.status(204).end();
   } catch (exception) {
-    console.log(exception)
-    response.status(400).send({ error: 'malformatted id' })
+    console.log(exception);
+    response.status(400).send({ error: 'malformatted id' });
   }
-})
+});
 
-module.exports = blogsRouter
+module.exports = blogsRouter;
--- bloglist/models/blog.js
+++ bloglist/models/blog.js
@@ -1,26 +1,28 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
-const url = process.env.MONGODB_URI
-mongoose.connect(url)
+const mongoose = require('mongoose');
+require('dotenv').config();
+const url = process.env.MONGODB_URI;
+mongoose.connect(url);
 
-const blogSchema = mongoose.Schema( {
+const blogSchema = mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number,
+  comments: [String],
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-})
+});
 
-blogSchema.statics.format = (blog) => {
-	return {
-		id: blog._id,
-		title: blog.title,
-		author: blog.author,
-		url: blog.url,
-		likes: blog.likes,
-		user: blog.user
-	}
-}
+blogSchema.statics.format = blog => {
+  return {
+    id: blog._id,
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes,
+    user: blog.user,
+    comments: blog.comments
+  };
+};
 
-const Blog = mongoose.model('Blog', blogSchema)
-module.exports = Blog
+const Blog = mongoose.model('Blog', blogSchema);
+module.exports = Blog;
